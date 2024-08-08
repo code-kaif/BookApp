5597addafb9a0da873a78ab9cba9928e494ac6e6
Kaif <kaif98747@gmail.com>
2024-08-08 16:06:26 +0530

    something

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
 
-export const url = "http://localhost:4001";
+export const url = "https://bookapp-1-nuya.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
